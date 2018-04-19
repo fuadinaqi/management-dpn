f4934e9a6ac49bfe9aaebf848da01ca22b581903
fuadinaqi <fuadinaqi@aisifoundation.org>
2018-04-19 22:00:01 +0700

    update landing page and stack navigator

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { StackNavigator } from 'react-navigation'
+import { Provider } from 'react-redux'
+import { Container, Text, Root } from 'native-base'
+// import store from './src/store'
 
-export default class App extends React.Component {
+import LandingPage from './src/components/landingPage'
+
+const AppNavigator = StackNavigator({
+  LandingPage: {
+    screen: LandingPage
+  }
+})
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <Container>
+        <LandingPage />
+      </Container>
+    )
   }
 }
 
---
+++ src/components/landingPage/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { Container, Text } from 'native-base'
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <Container>
+        <Text>Please login first</Text>
+      </Container>
+    );
+  }
+}
+
+export default LandingPage
